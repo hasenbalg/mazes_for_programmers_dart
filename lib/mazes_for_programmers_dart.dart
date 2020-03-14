b023a0edcfd259122c4627424b53fb282d15b9fb
frank <frankam@inbox.lv>
2020-03-14 20:36:13 +0100

    add timing

--- lib/mazes_for_programmers_dart.dart
+++ lib/mazes_for_programmers_dart.dart
@@ -21,21 +21,31 @@ void run() {
   var tries = 50;
   var size = 20;
 
-  Map<Algorithm, num> averages;
+  Map<Algorithm, List<num>> averages;
   averages = {};
   for (var algorythm in algorithms) {
     print('running $algorythm');
 
     List<int> deadEndCounts;
     deadEndCounts = [];
+    List<int> timingCounter;
+    timingCounter = [];
     for (var i = 0; i < tries; i++) {
+      var startTime = DateTime.now().microsecondsSinceEpoch;
       var grid = Grid(size, size);
       algorythm.on(grid);
+      var endTime = DateTime.now().microsecondsSinceEpoch;
+      timingCounter.add(endTime - startTime);
       deadEndCounts.add(grid.deadEnds().length);
     }
     int totalDeadEnds;
     totalDeadEnds = deadEndCounts.fold(0, (a, b) => a + b);
-    averages[algorythm] = totalDeadEnds / deadEndCounts.length;
+    int totalTime;
+    totalTime = timingCounter.fold(0, (a, b) => a + b);
+    averages[algorythm] = [
+      totalDeadEnds / deadEndCounts.length,
+      totalTime / timingCounter.length
+    ];
   }
 
   var totalCells = size * size;
@@ -45,21 +55,11 @@ void run() {
       'Average dead ends per ${size}x${size} maze (${size * size} total cells):');
   print('');
 
-  // var sortedKeys = averages.keys.toList(growable: false)
-  //   ..sort((k1, k2) => averages[k1].compareTo(averages[k2]));
-  // var sortedAlgorithms = LinkedHashMap.fromIterable(sortedKeys,
-  //     key: (k) => k, value: (k) => averages[k]);
-
-  // for (var algorithm in sortedAlgorithms.entries) {
-  // for (var algorithm in averages.entries) {
-  //   var value = averages[algorithm];
-  //   var percentage = value * 100 / (size * size);
-  //   print(
-  //       '${algorithm.key}: ${averages[algorithm]}, ${totalCells}, ${percentage}');
-  // }
-  averages.forEach((algorithm, number) {
-    var percentage = number * 100 / (size * size);
+  averages.forEach((algorithm, numbers) {
+    var percentage = numbers[0] * 100 / (size * size);
+    var time = numbers[1].toString().padLeft(6, ' ');
+    var deadEnds = numbers[0].toString().padLeft(6, ' ');
     print(
-        '${algorithm.toString().split('\'')[1]}: \t${number}/\t${totalCells},\t${percentage} %');
+        '${algorithm.toString().split('\'')[1]}: \t ${deadEnds} / \t ${totalCells}, \t ${percentage} % \t ${time} microsec');
   });
 }
